29d76521abc24871932cc2e78e7d88fc5add9cef
mefool <vicentegarciafernandes@tecnico.ulisboa.pt>
2020-08-03 19:43:09 +0000

    problem 3 Reshape Matrix

---
+++ leetcode/cpp/0556a_reshape_the_matrix.cpp
@@ -0,0 +1,37 @@
+/* Leetcode - Problem 3 - Reshape the Matrix - https://leetcode.com/problems/reshape-the-matrix/
+   In MATLAB, there is a very useful function called 'reshape', which can 
+   reshape a matrix into a new one with different size but keep its 
+   original data.
+
+   You're given a matrix represented by a two-dimensional array, and two 
+   positive integers r and c representing the row number and column number 
+   of the wanted reshaped matrix, respectively.
+
+   g++ 0566a_reshape_the_matrix.cpp -o reshape_matrix.exe
+
+   References:
+   > https://stackoverflow.com/questions/12375591/vector-of-vectors-to-create-matrix
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int m = nums.size(), n = nums[0].size();
+        if (m * n != r * c || r < 1 || c < 1) {
+            return nums;
+        }
+
+        vector<vector<int>> res(r, vector<int>(c, 0));
+        
+        for (int i = 0, k = 0; i < m; i++)
+            for (int j = 0; j < n; j++, k++)
+                res[k / c][k % c] = nums[i][j];
+
+        return res;
+        
+    }
+};
